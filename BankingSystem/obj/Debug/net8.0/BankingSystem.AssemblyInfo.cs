a7151f9abd369b205d13aa74306268e3171fb221
Junior Rodriguez <miguel.ajr89@gmail.com>
2024-07-24 16:17:42 -0400

    Last build and run

--- BankingSystem/obj/Debug/net8.0/BankingSystem.AssemblyInfo.cs
+++ BankingSystem/obj/Debug/net8.0/BankingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9150581e0e8f68f222380e487df0ffbb75c54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b07621e179bb6b6bf68cc296abba3c33f89a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
